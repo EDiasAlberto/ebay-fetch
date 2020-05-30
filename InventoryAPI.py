633af7c9621cb51b9c8fe70dfda1ee71d3f3f5c5
Retr0fade <retr0fade111@gmail.com>
2020-05-30 13:20:31 +0100

    remove unnecessary files

--- InventoryAPI.py
+++
@@ -1,5 +0,0 @@
-from ebaysdk.shopping import Connection
-api = Connection(config_file = "ebay.yaml", siteid = "EBAY-GB")
-response = api.execute('FindProducts', {'QueryKeywords': 'Ghost BCAA'})
-
-print(response.dict())

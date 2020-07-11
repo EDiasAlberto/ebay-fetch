1f7ab0e30377936e484dea75a7d27758cd94d047
Retr0fade <retr0fade111@gmail.com>
2020-07-11 15:09:46 +0100

    big update, loading files and creating listing objects

--- findingAPI.py
+++ findingAPI.py
@@ -1,7 +1,29 @@
 from ebaysdk.finding import Connection as Finding
 from ebaysdk.trading import Connection as Trading
+from decimal import Decimal
 
-'''findingApi = Finding(config_file = "ebay.yaml", siteid = "EBAY-GB")
+class ebayListingItem:
+    def __init__(self, name, cost, postage, actual, initStock, inStock, packaging=0.5):
+        self.name=name
+        self.cost=round(float(cost),2)
+        self.postage=round(float(postage),2)
+        self.packaging=round(float(packaging),2)
+        self.actual=round(float(actual),2)
+        self.ebay=round((int(self.actual*100)*0.11)/100, 2)
+        self.paypal=round(float((self.actual/100)*5),2)
+        self.minimum=round(self.ebay+self.paypal+self.cost+self.postage+self.packaging, 2)
+        if int(inStock)>0:
+            self.active=True
+        self.profit=round(float(self.actual-self.minimum),2)
+        self.initStock=int(initStock)
+        self.inStock=int(inStock)
+        self.soldStock=self.initStock-self.inStock
+
+    def printInfo(self):
+        print(self.name, self.cost, self.postage, self.packaging, self.actual, self.ebay, self.paypal, self.minimum, self.active, self.profit, self.initStock, self.soldStock, self.inStock)
+
+'''
+findingApi = Finding(config_file = "ebay.yaml", siteid = "EBAY-GB")
 
 requests ={
         "itemFilter": [
@@ -30,10 +52,14 @@ requests = {
 }
 
 counter=0
+fetchedListings=[]
 results = tradingApi.execute("GetMyeBaySelling", requests)
 for x in results.reply.ActiveList.ItemArray.Item:
-    print(f'Item name: {x.Title} Item price: £{x.SellingStatus.CurrentPrice.value} Stock Remaining: {x.QuantityAvailable}')
+    #print(f'Item name: {x.Title} Item price: £{x.SellingStatus.CurrentPrice.value} Initial Stock: {x.Quantity} Stock Remaining: {x.QuantityAvailable} Sold Stock {int(x.Quantity)-int(x.QuantityAvailable)}')
+    fetchedListings.append([x.Title, x.SellingStatus.CurrentPrice.value, x.Quantity, x.QuantityAvailable, int(x.Quantity)-int(x.QuantityAvailable)])
     counter+=1
+
+print()
 
 existingData=[]
 line = outputFile.readline()
@@ -42,5 +68,30 @@ while line!="":
     line=outputFile.readline()
 outputFile.close()
 
-for x in existingData:
-    print(x)
+modifiedExistingData=[]
+header=existingData.pop(0)
+for x in range(len(existingData)):
+    if not("n/a" in existingData[x]):
+        modifiedExistingData.append(existingData[x])
+
+for x in modifiedExistingData:
+    for y in range(len(x)):
+        x[y]=x[y].replace("Â£","")
+
+print()
+fetchedListings=sorted(fetchedListings, key = lambda x:x[0])
+modifiedExistingData=sorted(modifiedExistingData, key = lambda x:x[0])
+
+ebayObjects=[]
+
+
+
+for x in fetchedListings:
+    for y in modifiedExistingData:
+        if x[0]==y[0]:
+            ebayObjects.append(ebayListingItem(x[0], y[1], 3.70, x[1], initStock=x[2], inStock=x[3]))
+
+print("%-100s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-15s %-15s %-15s %-15s \n"%("Name", "Cost", "Postage", "Packaging", "EBay", "PayPal", "Minimum", "Actual", "Profit", "Initial Stock", "Sold Stock", "In Stock"))
+
+for x in ebayObjects:
+    print("%-100s %-10s %-10s %-10s %-10s %-10s %-10s %-10s %-15s %-15s %-15s %-15s \n"%(x.name, x.cost, x.postage, x.packaging, x.ebay, x.paypal, x.minimum, x.actual, x.profit, x.initStock, x.soldStock, x.inStock))
